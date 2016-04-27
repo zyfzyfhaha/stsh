e3dc361e2d037c0b89414d73f3a16a60422b34fa
SyberToto <xst0001@gmail.com>
2016-04-27 03:42:57 -0700

    first commit

---
+++ set_rvm/set_rvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -v on
+
+curl -L https://get.rvm.io | bash -s stable --autolibs=enabled
+
+source ~/.bashrc
+source ~/.bash_profile
+
+#Modify the rvm source to ruby-china
+
+
+#Modify the rvm source to taobao
+#sed -i -e 's/ftp\.ruby-lang\.org\/pub\/ruby/ruby\.taobao\.org\/mirrors\/ruby/g' ~/.rvm/config/db
