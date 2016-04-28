59461cf0cdbcd65e9e7c66b7ae22471fbd7e8601
SyberToto <xst0001@gmail.com>
2016-04-28 03:08:55 -0700

    merge set_vim

---
+++ set_vim/.my_vimrc/README.md
@@ -0,0 +1,9 @@
+#.my_vimrc
+
+This is my .vimrc file with [spf13-vim] support
+
+###Step to setup:
+1. clone the repo under home folder
+2. run setup.sh
+
+[spf13-vim]:https://github.com/spf13/spf13-vim
---
+++ set_vim/set_vim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -rf .my_vimrc ~/
+
+ln -s ~/.my_vimrc/.vimrc.local ~/.vimrc.local
+ln -s ~/.my_vimrc/.vimrc.before.local ~/.vimrc.before.local
+ln -s ~/.my_vimrc/.vimrc.bundles.local ~/.vimrc.bundles.local
+
+sh <(curl https://j.mp/spf13-vim3 -L)
