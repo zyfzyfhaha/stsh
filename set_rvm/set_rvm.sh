13cc7eca4f37d5ee73522a3bac878652b15a2904
SyberToto <xst0001@gmail.com>
2016-04-28 03:46:09 -0700

    modify comments for set rvm

--- set_rvm/set_rvm.sh
+++ set_rvm/set_rvm.sh
@@ -6,8 +6,5 @@ curl -L https://get.rvm.io | bash -s stable --autolibs=enabled
 source ~/.bashrc
 source ~/.bash_profile
 
-#Modify the rvm source to ruby-china
-
-
-#Modify the rvm source to taobao
+#Modify the rvm ruby source to taobao
 #sed -i -e 's/ftp\.ruby-lang\.org\/pub\/ruby/ruby\.taobao\.org\/mirrors\/ruby/g' ~/.rvm/config/db
