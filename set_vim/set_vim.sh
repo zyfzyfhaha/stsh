b8e61679fe5081bd45e33a7e2b684578024c2dff
SyberToto <xst0001@gmail.com>
2016-09-19 02:54:01 -0700

    modify script to include gvimrc

--- set_vim/set_vim.sh
+++ set_vim/set_vim.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm ~/.vimrc*
+rm ~/.*vimrc*
 cp -rf .my_vimrc ~/
 
 ln -s ~/.my_vimrc/.vimrc.local ~/.vimrc.local
 ln -s ~/.my_vimrc/.vimrc.before.local ~/.vimrc.before.local
 ln -s ~/.my_vimrc/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/.my_vimrc/.gvimrc ~/.gvimrc
 
 sh <(curl https://j.mp/spf13-vim3 -L)
