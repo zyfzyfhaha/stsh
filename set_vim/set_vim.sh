5430ff15fa3b0f9c14e00779e6880c71d0598cff
sybertoto <xst0001@gmail.com>
2016-09-19 18:36:44 +0800

    add bashrc

--- set_vim/set_vim.sh
+++ set_vim/set_vim.sh
@@ -6,6 +6,7 @@ cp -rf .my_vimrc ~/
 ln -s ~/.my_vimrc/.vimrc.local ~/.vimrc.local
 ln -s ~/.my_vimrc/.vimrc.before.local ~/.vimrc.before.local
 ln -s ~/.my_vimrc/.vimrc.bundles.local ~/.vimrc.bundles.local
-ln -s ~/.my_vimrc/.gvimrc ~/.gvimrc
 
 sh <(curl https://j.mp/spf13-vim3 -L)
+
+ln -s ~/.my_vimrc/.gvimrc ~/.gvimrc
