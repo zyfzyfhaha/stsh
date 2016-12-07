2531abe42658e6bff602a8275d135bbd1dda7713
SyberToto <xst0001@gmail.com>
2016-12-07 04:45:47 -0800

    add gpg key for rvm

--- set_rvm/set_rvm.sh
+++ set_rvm/set_rvm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -v on
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
 curl -L https://get.rvm.io | bash -s stable --autolibs=enabled
 
 source ~/.bashrc
