3b71c124515b493b8bfb61e5ec27cbc23505ac52
tshen <tshen@microstrategy.com>
2016-12-13 02:56:09 +0000

    Add command to remove git editor and use VIM instead

--- set_win_env.bat
+++ set_win_env.bat
@@ -1,2 +1,6 @@
+REM This is only used for company debug VM
+
 setx PATH "%USERPROFILE%\bin"
 setx HOME "%USERPROFILE%"
+
+REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V GIT_EDITOR
