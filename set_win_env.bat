ff533fa1f7a12ab100a289f3b5a4aede6c0d16ef
tshen <tshen@microstrategy.com>
2016-12-13 04:04:20 +0000

    force set git editor to VIM

--- set_win_env.bat
+++ set_win_env.bat
@@ -3,4 +3,5 @@ REM This is only used for company debug VM
 setx PATH "%USERPROFILE%\bin"
 setx HOME "%USERPROFILE%"
 
-REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V GIT_EDITOR
+REM REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V GIT_EDITOR
+setx /M GIT_EDITOR VIM
