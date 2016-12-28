b61a0333ac59f98a061ef36c78a92cea31e4f872
SyberToto <xst0001@gmail.com>
2016-12-28 02:34:32 -0800

    remove company mail address

--- win_env.bat
+++
@@ -1,7 +0,0 @@
-REM This is only used for company debug VM
-
-setx PATH "%USERPROFILE%\bin"
-setx HOME "%USERPROFILE%"
-
-REM REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V GIT_EDITOR
-setx /M GIT_EDITOR VIM
