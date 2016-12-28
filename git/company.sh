d46810d8aa96eb14060e4d912dd95acb7dbcc1f2
tshen <tshen@microstrategy.com>
2016-12-28 09:59:49 +0000

    update google repo

--- git/company.sh
+++ git/company.sh
@@ -6,4 +6,8 @@ git config --global user.email tshen@microstrategy.com
 
 git config --global color.ui auto && git config --global core.autocrlf true && git config --global core.protectNTFS false && git config --global core.longpaths true && git config --global credential.helper wincred
 
-repo init
+curl http://ctc-infrastructure/yuyang/google_repo/raw/compatible/repo > /c/google_repo/repo
+
+repo init -u https://github.microstrategy.com/Tech/manifest -m server.xml
+
+repo sync
